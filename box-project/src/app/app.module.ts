abcc3edb1b46a026ddd2463cac8363a1de016731
Lordstacker <nicoenrique_1994@hotmail.com>
2023-10-03 11:03:52 +0200

    navbar and card added also the routing module

--- box-project/src/app/app.module.ts
+++ box-project/src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {RouterLink, RouterLinkActive, RouterModule, RouterOutlet, Routes} from "@angular/router";
 
 import { AppComponent } from './app.component';
+import { IonicModule } from '@ionic/angular';
+import { CardComponent } from './card/card.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+
+const routes: Routes = [
+  { path: 'home', component: CardComponent },
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    [RouterModule.forRoot(routes)],
+    BrowserModule,
+    IonicModule.forRoot()
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [RouterModule]
 })
 export class AppModule { }
