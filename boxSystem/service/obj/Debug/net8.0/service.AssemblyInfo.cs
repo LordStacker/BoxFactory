61a4d6899c5f606b839a624ee56f8c125cb0f3cd
Lordstacker <nicoenrique_1994@hotmail.com>
2023-10-10 15:35:06 +0200

    test 2

--- boxSystem/service/obj/Debug/net8.0/service.AssemblyInfo.cs
+++ boxSystem/service/obj/Debug/net8.0/service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bf27372ef393a000287246ea7554ae12dc4bfab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
